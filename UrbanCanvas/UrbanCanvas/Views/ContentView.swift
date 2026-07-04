36e383f3387ac872390adf4603f151ccb9c2f49e
djeska3 <djeska3@gmail.com>
2026-07-04 15:41:46 +0200

    forgotten commit from yesterday (oops)

--- UrbanCanvas/UrbanCanvas/Views/ContentView.swift
+++ UrbanCanvas/UrbanCanvas/Views/ContentView.swift
@@ -10,15 +10,55 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        NavigationStack{
-            VStack(alignment : .leading){
-                List(streetarts){ streetart in
-                    StreetArtRowView(streetart: streetart, isPictured: false)
+        
+            VStack{
+                
+                HStack{
+                    Spacer()
+                    Button{
+                        
+                    }label: {
+                        ZStack{
+                            Circle()
+                                .frame(width: 45)
+                                .foregroundStyle(.white)
+                            
+                            Image(systemName: "line.3.horizontal.decrease.circle")
+                                .font(.system(size: 32))
+                                .padding(7)
+                        }
+                        
+                        
+                    }
+                    .tint(.black)
+                    .glassEffect(.clear)
+                    
+                }
+                .padding()
+                
+                NavigationStack{
+                    
+                    VStack(alignment : .leading){
+                        
+                        List(streetarts){ streetart in
+                            StreetArtRowView(streetart: streetart, isPictured: false)
+                            
+                        }
+                        .scrollContentBackground(.hidden)
+                        .padding(.top,10)
+                        
+                        
+                        
+                    }
+                    .navigationTitle("Liste des Street arts")
+                    .background(.quinary)
                     
                 }
             }
-        }
+            .background(.quinary)
+        
     }
+        
 }
 
 #Preview {
