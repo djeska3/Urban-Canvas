ca3f9e664f23f61f4156f06faf0116c8efe6aade
djeska3 <djeska3@gmail.com>
2026-07-07 15:41:13 +0200

    V2 filtre

--- UrbanCanvas/UrbanCanvas/Views/ContentView.swift
+++ UrbanCanvas/UrbanCanvas/Views/ContentView.swift
@@ -11,69 +11,61 @@ struct ContentView: View {
     
     @State private var isPushed = false
     @State private var showFilter = false
+    @State var selectedType : String = "Tous"
+     var filteredList: [StreetArt] {
+             streetarts.filter { $0.type == selectedType }
+    }
+    
     
     var body: some View {
         
         NavigationStack{
-            
-            VStack(alignment : .leading){
-                
-                Text("Liste des Street arts")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.leading)
-                List(streetarts){ streetart in
-                    StreetArtRowView(streetart: streetart, isPictured: false)
+            ZStack {
+                VStack(alignment : .leading){
                     
+                    Text("Liste des Street arts")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.leading)
+                    List(selectedType == "Tous" ? streetarts : filteredList ){ streetart in
+                        StreetArtRowView(streetart: streetart, isPictured: false)
+                        
+                    }
+                    .scrollContentBackground(.hidden)
+                    
+                    if showFilter {
+                        
+                        
+//                                            streetarts.filter{ 0.type == $selectedType}
+                        
+                        ZStack{
+                                               FilterTypeView(selectedType: $selectedType)
+                        }
+                        
+                    }
                 }
-                .scrollContentBackground(.hidden)
-                
             }
-            .background(.quinary)
             .toolbar {
                 ToolbarItem (placement: .primaryAction){
                     Button{
-                        
-                        if isPushed {
-                            showFilter = true
-                            
-                        } else {
-                          
-                        }
-                        isPushed.toggle()
+                        showFilter.toggle()
                     }label: {
                         ZStack{
-                            
                             Image(systemName: "line.3.horizontal.decrease.circle")
                                 .font(.system(size: 28))
                                 .padding(7)
                                 .foregroundStyle(.black)
                         }
-                     
-                        
-                        
-                     
                     }
-                    
                 }
-            
             }
-           
-            if showFilter {
-                
-                ZStack{
-                    FilterTypeView()
-                    
-                }
-           
-            }
-
-            
         }
         .background(.quinary)
+        
     }
 }
+//        .background(.quinary)
 
 #Preview {
-    ContentView()
+    ContentView(selectedType: "Tous")
 }
--- UrbanCanvas/UrbanCanvas/Views/FilterTypeView.swift
+++ UrbanCanvas/UrbanCanvas/Views/FilterTypeView.swift
@@ -10,144 +10,157 @@ import SwiftUI
 
 struct FilterTypeView: View {
     
-    @State private var isPushed = false
-    @State private var isSelected = false
-   
-    @State private var types = ["Tous",
-                                "Invader",
-                                "Mosaïque",
-                                "Mural",
-                                "Stencil",
-                                "Calligraphie",
-                                "Harlftone"]
+    @Binding var selectedType : String 
+    
+    var types = ["Tous",
+                 "Invader",
+                 "Mosaïque",
+                 "Mural",
+                 "Stencil",
+                 "Calligraphie",
+                 "Harlftone"]
     
     var body: some View {
         
-        VStack{
-  
-                    ZStack{
-                   
-                 
-                        RoundedRectangle(cornerRadius: 32)
-                            .frame(width: 300, height: 500)
-                            .foregroundStyle(.white.opacity(0.2))
-                           
-                        VStack(alignment: .leading, spacing: 7){
-                            
-                            Text("Liste des filtres")
-                                .fontWeight(.semibold)
-                                .padding(.leading,10)
-                                .foregroundStyle(.black)
+        
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 32)
+                    .frame(width: 300, height: 500)
+                    .foregroundStyle(.white.opacity(0.2))
+            
+                VStack(alignment: .leading, spacing: 7){
+                    
+                    Text("Liste des filtres")
+                        .fontWeight(.semibold)
+                        .padding(.leading,10)
+                        .foregroundStyle(.black)
+                    
+                    Text("Choisissez un type d'art")
+                        .padding(10)
+                        .foregroundStyle(.black)
+                    
+                    ForEach(types, id: \.self) { type in
+                        ZStack{
+                            Capsule()
+                                .frame(width: 270, height: 50)
+                                .foregroundStyle(.gray.opacity(0.2))
+                            Button {
+                                selectedType = type
+                                                               
                                 
-                            Text("Choisissez un type d'art")
-                                .padding(10)
-                                .foregroundStyle(.black)
-                                
-                                
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Tous")
-                                        .foregroundStyle(.red)
-                                        .font(.title2)
-                                }
+                            } label : {
+                                Text(type)
+                                    .foregroundStyle(selectedType == type  ? .red : .black)
+                                    .font(.title2)
                             }
-                            
-                            Button{
-                                if isSelected{
-                                   
-                                }
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Invaders")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-                            
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Mosaïques")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-                            
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Mural")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-                            
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Stencil")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-                            
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Calligraphie")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-                            
-                            Button{
-                                
-                            }label: {
-                                ZStack{
-                                    Capsule()
-                                        .frame(width: 270, height: 50)
-                                        .foregroundStyle(.gray.opacity(0.2))
-                                    Text("Harlftone")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                }
-                            }
-
-
                         }
                     }
+                }
+                    //                            Button{
+                    //
+                    //                            }label: {
+//                                                    ZStack{
+//                                                        Capsule()
+//                                                            .frame(width: 270, height: 50)
+//                                                            .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Tous")
+//                                                            .foregroundStyle(.red)
+//                                                            .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //                                if isSelected{
+                    //
+                    //                                }
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Invaders")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Mosaïques")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Mural")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Stencil")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Calligraphie")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //                            Button{
+                    //
+                    //                            }label: {
+                    //                                ZStack{
+                    //                                    Capsule()
+                    //                                        .frame(width: 270, height: 50)
+                    //                                        .foregroundStyle(.gray.opacity(0.2))
+                    //                                    Text("Harlftone")
+                    //                                        .foregroundStyle(.black)
+                    //                                        .font(.title2)
+                    //                                }
+                    //                            }
+                    //
+                    //
+                    //                        }
+                }
                 
-           // }
-          
+                
+                
+            }
+            
         }
-        
+    
+    #Preview {
+        FilterTypeView(selectedType: .constant("Tous"))
     }
-}
-#Preview {
-    FilterTypeView()
-}
